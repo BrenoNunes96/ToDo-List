3459b62105e46f7c6ad115dd035db60154538f4e
Breno <147736256+BrenoNunes96@users.noreply.github.com>
2024-03-13 12:11:10 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
+</script>
+
+<template>
+  <div>
+    <Todo/>
+
+  </div>
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="container">
+
+<h1>TO DO LIST</h1>
+  
+  <input class="input" v-model="novaTarefa" @keyup.enter="adicionarTarefa" placeholder="Adicione aqui">
+  <button class="botao2" @click="adicionarTarefa"> adicionar Tarefa</button>
+  <ul>
+    <li class="lista" v-for="(tarefa, indice) in tarefas" :key="indice">
+      {{ tarefa }}
+      <button class="botao1" @click="removerTarefa(indice)">Remover</button>
+    </li>
+  </ul>
+</div>
+
+
+</template>
+    
+<script>
+export default{
+
+data() {
+        return{
+      novaTarefa: '',
+      tarefas: []
+    }},
+    methods: {
+      adicionarTarefa: function () {
+        if (this.novaTarefa.trim() !== '') {
+          this.tarefas.push(this.novaTarefa.trim());
+          this.novaTarefa = '';
+        }
+      },
+      removerTarefa: function (indice) {
+        this.tarefas.splice(indice, 1);
+      }
+    }
+  };
+
+</script>
+
+<style>
+.input{
+    height: 50px;
+    width: 300px;
+    border-radius: 15px;
+
+}
+.container{
+margin-bottom: 500px;
+}
+
+h1{
+text-align: center;
+}
+
+.botao2{
+    margin-left: 20px;
+}
+ 
+
+
+
+</style>
